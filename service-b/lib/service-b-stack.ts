4b4735dff98b7c6a5861e8f9533024c458a5366e
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2023-08-24 10:49:46 +0200

    remove graphql and resolvers from service b

--- service-b/lib/service-b-stack.ts
+++ service-b/lib/service-b-stack.ts
@@ -10,9 +10,9 @@ export class ServiceBStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const tableName = "serviceAItems";
+    const tableName = "serviceBItems";
 
-    const itemsTable = new Table(this, "serviceAItems", {
+    const itemsTable = new Table(this, "serviceBItems", {
       tableName: tableName,
       partitionKey: {
         name: `${tableName}Id`,
@@ -24,7 +24,7 @@ export class ServiceBStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    const itemsTableRole = new Role(this, "ServiceAItemsDynamoDBRole", {
+    const itemsTableRole = new Role(this, "serviceBItemsDynamoDBRole", {
       assumedBy: new ServicePrincipal("appsync.amazonaws.com"),
     });
 
@@ -32,8 +32,8 @@ export class ServiceBStack extends cdk.Stack {
       ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess")
     );
 
-    const itemsGraphQLApi = new CfnGraphQLApi(this, "serviceAApi", {
-      name: "serviceAApi",
+    const itemsGraphQLApi = new CfnGraphQLApi(this, "serviceBApi", {
+      name: "serviceBApi",
       authenticationType: "API_KEY",
     });
 
@@ -43,7 +43,7 @@ export class ServiceBStack extends cdk.Stack {
       exportName: 'apiUrl',
     });
 
-    const apiKey = new CfnApiKey(this, "serviceAApiKey", {
+    const apiKey = new CfnApiKey(this, "serviceBApiKey", {
       apiId: itemsGraphQLApi.attrApiId,
     });
 
@@ -52,110 +52,5 @@ export class ServiceBStack extends cdk.Stack {
       description: 'api key',
       exportName: 'apiKey',
     });
-
-    const apiSchema = new CfnGraphQLSchema(this, "serviceAschema", {
-      apiId: itemsGraphQLApi.attrApiId,
-      definition: `type ${tableName} {
-        ${tableName}Id: ID!
-        name: String
-      }
-      type Paginated${tableName} {
-        items: [${tableName}!]!
-        nextToken: String
-      }
-      type Query {
-        all(limit: Int, nextToken: String): Paginated${tableName}!
-        getOne(${tableName}Id: ID!): ${tableName}
-      }
-      type Mutation {
-        save(name: String!): ${tableName}
-        delete(${tableName}Id: ID!): ${tableName}
-      }
-      type Schema {
-        query: Query
-        mutation: Mutation
-      }`,
-    });    
-
-    const dataSource = new CfnDataSource(this, "ServiceAItemsDataSource", {
-      apiId: itemsGraphQLApi.attrApiId,
-      name: "ServiceAItemsDataSource",
-      type: "AMAZON_DYNAMODB",
-      dynamoDbConfig: {
-        tableName: itemsTable.tableName,
-        awsRegion: this.region,
-      },
-      serviceRoleArn: itemsTableRole.roleArn,
-    });
-
-    const getOneResolver = new CfnResolver(this, "GetOneQueryResolver", {
-      apiId: itemsGraphQLApi.attrApiId,
-      typeName: "Query",
-      fieldName: "getOne",
-      dataSourceName: dataSource.name,
-      requestMappingTemplate: `{
-        "version": "2017-02-28",
-        "operation": "GetItem",
-        "key": {
-          "${tableName}Id": $util.dynamodb.toDynamoDBJson($ctx.args.${tableName}Id)
-        }
-      }`,
-      responseMappingTemplate: `$util.toJson($ctx.result)`,
-    });
-    getOneResolver.addDependency(apiSchema);
-    getOneResolver.addDependency(dataSource);
-
-    const getAllResolver = new CfnResolver(this, "GetAllQueryResolver", {
-      apiId: itemsGraphQLApi.attrApiId,
-      typeName: "Query",
-      fieldName: "all",
-      dataSourceName: dataSource.name,
-      requestMappingTemplate: `{
-        "version": "2017-02-28",
-        "operation": "Scan",
-        "limit": $util.defaultIfNull($ctx.args.limit, 20),
-        "nextToken": $util.toJson($util.defaultIfNullOrEmpty($ctx.args.nextToken, null))
-      }`,
-      responseMappingTemplate: `$util.toJson($ctx.result)`,
-    });
-    getAllResolver.addDependency(apiSchema);
-    getAllResolver.addDependency(dataSource);
-
-    const saveResolver = new CfnResolver(this, "SaveMutationResolver", {
-      apiId: itemsGraphQLApi.attrApiId,
-      typeName: "Mutation",
-      fieldName: "save",
-      dataSourceName: dataSource.name,
-      requestMappingTemplate: `{
-        "version": "2017-02-28",
-        "operation": "PutItem",
-        "key": {
-          "${tableName}Id": { "S": "$util.autoId()" }
-        },
-        "attributeValues": {
-          "name": $util.dynamodb.toDynamoDBJson($ctx.args.name)
-        }
-      }`,
-      responseMappingTemplate: `$util.toJson($ctx.result)`,
-    });
-    saveResolver.addDependency(apiSchema);
-    saveResolver.addDependency(dataSource);
-
-    const deleteResolver = new CfnResolver(this, "DeleteMutationResolver", {
-      apiId: itemsGraphQLApi.attrApiId,
-      typeName: "Mutation",
-      fieldName: "delete",
-      dataSourceName: dataSource.name,
-      requestMappingTemplate: `{
-        "version": "2017-02-28",
-        "operation": "DeleteItem",
-        "key": {
-          "${tableName}Id": $util.dynamodb.toDynamoDBJson($ctx.args.${tableName}Id)
-        }
-      }`,
-      responseMappingTemplate: `$util.toJson($ctx.result)`,
-    });
-    deleteResolver.addDependency(apiSchema);
-    deleteResolver.addDependency(dataSource);
   }
 }
