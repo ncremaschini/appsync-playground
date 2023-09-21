34ff9516d12d82ee2d1e63da66027f0be1a0162e
Nicola Cremaschini <nicola.cremaschini@gmail.com>
2023-09-21 14:12:35 +0200

    fix merged api execution role

--- merged-api/lib/merged-api-stack.ts
+++ merged-api/lib/merged-api-stack.ts
@@ -1,5 +1,7 @@
 import * as appsync from 'aws-cdk-lib/aws-appsync';
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 import { Construct } from 'constructs';
@@ -11,11 +13,19 @@ export class MergedApiStack extends cdk.Stack {
     const { servicaAApId, servicaBApId } = this.getFederatedApiIds(); 
   
     const { serviceAApi, serviceBApi} = this.lookupApis(servicaAApId, servicaBApId);
+
+    const mergedApiExecutionRole = this.createExecutionRole();
   
     this.createDatasources(serviceAApi, serviceBApi);
+
+    const logConfig: appsync.LogConfig = {
+      retention: logs.RetentionDays.ONE_DAY,
+    };
     
     const api = new appsync.GraphqlApi(this, 'MergedApi', {
       name: 'merged-api',
+      logConfig,
+      
       definition: appsync.Definition.fromSourceApis({
         sourceApis: [
           {
@@ -27,6 +37,7 @@ export class MergedApiStack extends cdk.Stack {
             mergeType: appsync.MergeType.AUTO_MERGE,
           },
         ],
+        mergedApiExecutionRole: mergedApiExecutionRole
       }),
     }); 
   }
@@ -60,6 +71,22 @@ export class MergedApiStack extends cdk.Stack {
 
     serviceBApi.addNoneDataSource('ServiceBDS', {
       name: cdk.Lazy.string({ produce(): string { return 'ServiceBDS'; } }),
+    });
+  }
+
+  private createExecutionRole() {
+    return new iam.Role(this, 'MergedApiExecutionRole', {
+      assumedBy: new iam.ServicePrincipal('appsync.amazonaws.com'),
+      inlinePolicies: {
+        appsync: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              resources: ["*"],
+              actions: ["appsync:*"]
+            })
+          ]
+        })
+      }
     });
   }
 }
